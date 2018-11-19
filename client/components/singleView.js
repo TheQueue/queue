0ae8feda647c8155b34e41d9bf39042cfb7c6ae5
Guethie Kane <gkane6@student.cscc.edu>
2018-11-19 10:50:28 -0600

    Component for single B View

--- client/components/singleView.js
+++ client/components/singleView.js
@@ -5,29 +5,33 @@ import Button from '@material-ui/core/Button'
 import {getDetails} from '../store/business'
 
 function mapState(state) {
-  return {}
+  return {
+    business: state.business.single
+  }
 }
 function mapDispatch(dispatch) {
-  return {}
+  return {
+    getB: id => dispatch(getDetails(id))
+  }
 }
 
 class SingleBusiness extends React.Component {
   componentDidMount() {
-    this.props.getDetails(Number(this.props.match.params.id))
+    this.props.getB(Number(this.props.match.params.id))
   }
   componentWillUnmount() {}
   render() {
-    // if () return <div>No Product</div>
     console.log(this.props)
     return (
       <div className="sp">
-        <img src="" />
-        <h3 />
+        <img src={this.props.business.imageUrl} />
+        <h3>{this.props.business.name}</h3>
         <p />
+        Address: {this.props.business.address}
+        phoneNumber: {this.props.business.phoneNumber}
         <Button size="large" color="primary" variant="contained">
           Stufff
         </Button>
-
         <p />
       </div>
     )
