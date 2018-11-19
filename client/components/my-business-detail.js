00ac7059781e9a150412a504c3e017e0d151dbd1
David Jung <djung31@gmail.com>
2018-11-19 14:08:54 -0600

    refactored for improved clarity in my-business-detail

--- client/components/my-business-detail.js
+++ client/components/my-business-detail.js
@@ -18,6 +18,9 @@ class MyBusinessDetail extends Component {
   render() {
     const {myBusinesses} = this.props
     const {businessId} = this.state
+
+    // we'll loop through this array to render matching reservations
+    const statusList = ['Pending', 'Active', 'Serviced', 'Cancelled', 'Holding']
     // if isLoading value is true -> loading msg
     if (myBusinesses.isLoading) {
       return (
@@ -32,6 +35,16 @@ class MyBusinessDetail extends Component {
       const {entities, result} = myBusinesses.businessData
       if (entities.businesses[businessId]) {
         const currBusiness = entities.businesses[businessId]
+
+        // matchingReservs is an object of arrays
+        // key = status, value = filtered list of reservs based on statusList entry
+        const matchingReservs = {};
+        statusList.forEach(status => {
+          matchingReservs[status] = entities.queues[currBusiness.queues[0]].reservations
+            .map(reservationId => entities.reservations[reservationId])
+            .filter(res => res.status === status)
+        })
+
         return (
           <div className="container">
             <div className="box">
@@ -44,59 +57,30 @@ class MyBusinessDetail extends Component {
               <div className="box">
                 <h2 className="title">Queue:</h2>
                 <h3>
-                  Queue Length:{' '}
-                  {entities.queues[currBusiness.queues[0]].queueLength}
+                  Active Queue Length:{' '}
+                  {matchingReservs.Active.length}
                 </h3>
-                <h3>Pending Reservations:</h3>
-                {entities.queues[currBusiness.queues[0]].reservations
-                  .length && (
-                  <div>
+                {statusList.map((status) => (
+                  <React.Fragment key={status}>
+                    <h3>
+                      {status} Reservations ({
+                        matchingReservs[status].length
+                      })
+                    </h3>
                     {entities.queues[currBusiness.queues[0]].reservations
-                      .map(
-                        reservationId => entities.reservations[reservationId]
-                      )
-                      .filter(res => res.status === 'Pending')
-                      .map(reservation => (
-                        <ReservationCard
-                          reservation={reservation}
-                          key={reservation.id}
-                        />
-                      ))}
-                  </div>
-                )}
-                <h3>Active Reservations:</h3>
-                {entities.queues[currBusiness.queues[0]].queueLength && (
-                  <div>
-                    {entities.queues[currBusiness.queues[0]].reservations.map(
-                      reservationId => {
-                        const reservation = entities.reservations[reservationId]
-                        return (
-                          <ReservationCard
-                            reservation={reservation}
-                            key={reservation.id}
-                          />
-                        )
-                      }
+                      .length && (
+                      <div>
+                        {matchingReservs[status]
+                          .map(reservation => (
+                            <ReservationCard
+                              reservation={reservation}
+                              key={reservation.id}
+                            />
+                          ))}
+                      </div>
                     )}
-                  </div>
-                )}
-                <h3>Serviced Reservations:</h3>
-                {entities.queues[currBusiness.queues[0]].reservations
-                  .length && (
-                  <div>
-                    {entities.queues[currBusiness.queues[0]].reservations
-                      .map(
-                        reservationId => entities.reservations[reservationId]
-                      )
-                      .filter(res => res.status === 'Serviced')
-                      .map(reservation => (
-                        <ReservationCard
-                          reservation={reservation}
-                          key={reservation.id}
-                        />
-                      ))}
-                  </div>
-                )}
+                  </React.Fragment>
+                ))}
               </div>
             ) : (
               'no queue'
