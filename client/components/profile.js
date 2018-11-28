e09a2c19a951915ae3ca7b31ae81378ce93b4c8f
Roy Yeh <royyeh007@gmail.com>
2018-11-28 14:37:33 -0600

    merge with master and profile page

--- client/components/profile.js
+++ client/components/profile.js
@@ -2,22 +2,11 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
 import {getDetails} from '../store/business'
-import {createNewReservation} from '../store/reservation'
+import {me} from '../store'
 import Navbar from './navbar/navbarMain'
+import PropTypes from 'prop-types'
 import Footer from './footer'
-function mapState(state) {
-  return {
-    user: state.user
-  }
-}
-function mapDispatch(dispatch) {
-  return {
-    getB: id => dispatch(getDetails(id)),
-    createNewReservation: reservationData => {
-      dispatch(createNewReservation(reservationData))
-    }
-  }
-}
+import moment from 'moment'
 
 class Profile extends React.Component {
   constructor(props) {
@@ -31,42 +20,53 @@ class Profile extends React.Component {
   }
 
   render() {
+    const {email, user} = this.props
+    console.log(user)
     return (
       <div>
         <Navbar />
 
-        <div className="insideFrame">
-          <div className="card">
-            <div className="card-image">
-              <figure className="image is-4by3">
+        <div className="insideFrame has-text-centered">
+          <div className="card is-centered">
+            <div className="card-image has-text-centered is-centered">
+              <figure className="image is-128x128 is-centered">
                 <img
-                  src="https://bulma.io/images/placeholders/1280x960.png"
+                  className="is-rounded"
+                  src="http://thehivedaily.com/wp-content/uploads/2018/09/crazy-hairstyles-cool-for-hair-day-hairstyle-tattoo.jpg"
                   alt="Placeholder image"
                 />
               </figure>
             </div>
             <div className="card-content">
               <div className="media">
-                <div className="media-left">
-                  <figure className="image is-48x48">
-                    <img
-                      src="https://bulma.io/images/placeholders/96x96.png"
-                      alt="Placeholder image"
-                    />
-                  </figure>
-                </div>
+                <div className="media-left" />
                 <div className="media-content">
-                  <p className="title is-4">John Smith</p>
-                  <p className="subtitle is-6">@johnsmith</p>
+                  <p className="title is-4">{user.username}</p>
+                  <p className="subtitle is-6">{user.email}</p>
+                  <p className="subtitle is-6">{user.phoneNumber}</p>
                 </div>
               </div>
 
               <div className="content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                <a href="#">#css</a> <a href="#">#responsive</a>
+                I love to have werid hair style, and I love this app.
                 <br />
-                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                <a>#animalstyle </a>
+                <a href="#">#longhair</a> <a href="#">#horseonhead</a>
+                <br />
+                <p>
+                  Joined since: {moment(user.createdAt).format('MMM Do YY')}
+                </p>
+              </div>
+              <div className="content has-text-centered">
+                <strong> Images</strong>
+                <figure className="image is-128x128 has-text-centered">
+                  <img
+                    className="is-rounded"
+                    src="https://i.pinimg.com/236x/08/ca/5e/08ca5e97a69b1ddcf074063eeba304ef--crazy-hairstyles-unique-hairstyles.jpg"
+                    alt="Placeholder image"
+                  />
+                </figure>
+
               </div>
             </div>
           </div>
@@ -76,5 +76,23 @@ class Profile extends React.Component {
     )
   }
 }
+function mapState(state) {
+  return {
+    user: state.user,
+    email: state.user.email
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    loadInitialData() {
+      dispatch(me())
+    }
+  }
+}
 
 export default connect(mapState, mapDispatch)(Profile)
+
+Profile.propTypes = {
+  email: PropTypes.string
+}
